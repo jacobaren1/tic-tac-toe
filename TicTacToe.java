8fa7410f3e0688d3e0173cba25f075ddfc8e32a1
Jacob Arén <jacob.aren.1@gmail.com>
2026-05-25 14:30:00 +0200

    TTT-1: Modified test method countMyArray

--- TicTacToe.java
+++ TicTacToe.java
@@ -24,12 +24,35 @@ public class TicTacToe {
     }
     
     public static void countMyArray(){
-        char[] myArray = {'X','O','X','0','0'};
+
+        // Input parameters
         
-        char myChar = 'X';
-        int myArrayCount = countXO(myArray, myChar);
-        System.out.println("My array with chars has " + myArrayCount + " " + myChar + ":s in a row");
+        char [][] twoDimArray = {
+            {'X','X','X','X','X'},
+            {'O','O','O','O','O'},
+            {'X','O','X','O','O'}
+        };
+
+        for (char[] myArray: twoDimArray){
+            
+            char myChar = 'X';
         
+            // Result
+            int inARowCount = countXO(myArray, myChar);
+            boolean isFiveInARow = isFiveInARow(myArray, myChar);
+            
+            // Print
+            String resultPrint = "";
+
+            if (isFiveInARow) {
+                resultPrint += "Congratulations, your array has (at least) 5 " + myChar + ":s in a row";
+            }
+            else {
+                resultPrint += "Keep it up! Your array have " + inARowCount  + " " + myChar + ":s in a row";
+            };
+            
+            System.out.println( resultPrint );
+        }
     };
 
     public static void main(String[] args){
