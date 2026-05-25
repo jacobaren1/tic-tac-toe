264bb5f386360715f5b063440bcf46d9b9ad30c4
Jacob Arén <jacob.aren.1@gmail.com>
2026-05-25 14:17:26 +0200

    TTT-1: Modified countXO to count no chars in a row

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,22 +1,35 @@
 public class TicTacToe {
 
     public static int countXO(char[] l, char c){
+        int maxInARow = 0;
         int countC = 0;
         for (char x: l){
             boolean charInListIsC = (x==c);
             if (charInListIsC){
                 countC ++;
+                maxInARow=Math.max(maxInARow, countC);
             }
-        }
-        return countC;
+            else {
+                countC = 0;
+            };
+        };
+
+        return maxInARow;
     };
+
+    public static boolean isFiveInARow(char[] l, char c){
+        int countC = countXO(l, c);
+
+        return (countC >= 5);
+    }
     
     public static void countMyArray(){
-        char[] myArray = {'0','0','X','0','0'};
+        char[] myArray = {'X','O','X','0','0'};
         
         char myChar = 'X';
         int myArrayCount = countXO(myArray, myChar);
-        System.out.println("My array with chars has " + myArrayCount + " " + myChar + ":s");
+        System.out.println("My array with chars has " + myArrayCount + " " + myChar + ":s in a row");
+        
     };
 
     public static void main(String[] args){
