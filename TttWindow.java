9c4c892aa00410d52e4cf60f160db2a37fb4b8df
Jacob Arén <jacob.aren.1@gmail.com>
2026-07-12 17:35:46 +0200

    TTT-8: Added a 'Start game'-button

--- TttWindow.java
+++ TttWindow.java
@@ -4,6 +4,8 @@ import java.awt.Button;
 import java.awt.Panel;
 import java.awt.GridLayout;
 import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,22 +17,18 @@ public class TttWindow {
     // main function
 
     private static class PlayerSection {
-
-        private Frame parent;
         public char boardChar;
+
         /**
          * Creates a part of a frame for as an aid to create a player
          *
-         * @param parent parent awt.frame
          * @param boarChar the players char (playing piece)
          */
-        public PlayerSection(Frame parent, char boardChar) {
-            this.parent = parent;
+        public PlayerSection(char boardChar) {
             this.boardChar = boardChar;
-        };
+        }
 
-        public void render() {
-            
+        public void render(Panel parentPanel) {
             Panel section = new Panel(new GridLayout(3, 1, 0, 10));
             Label inputHelp = new Label(String.format("Enter player name for player '%c'!", this.boardChar));
             TextField playerNameInput = new TextField(20);
@@ -58,29 +56,45 @@ public class TttWindow {
             section.add(inputRow);
             section.add(showPlayerName);
 
-            parent.add(section);
-        };
+            parentPanel.add(section);
+        }
+    }
 
-    };
     public static void main(String[] args)
     {
         // Declaring a Frame and Label
         Frame frame = new Frame("Tic-Tac-Toe main menu");
-        PlayerSection playerX = new PlayerSection( frame, 'X' );
-        PlayerSection playerO = new PlayerSection( frame, 'O' );
-        
-        frame.setLayout(new GridLayout(2, 1, 0, 20));
+        PlayerSection playerX = new PlayerSection('X');
+        PlayerSection playerO = new PlayerSection('O');
+        Button bStartGame = new Button("Start game!");
 
-        playerX.render();
-        playerO.render();
+        Panel playerPanel = new Panel(new GridLayout(2, 1, 0, 20));
+        Panel buttonPanel = new Panel(new FlowLayout(FlowLayout.CENTER));
+
+        bStartGame.setPreferredSize(new Dimension(180, 30));
+
+        playerX.render(playerPanel);
+        playerO.render(playerPanel);
+
+        frame.setLayout(new BorderLayout(0, 20));
+        frame.add(playerPanel, BorderLayout.CENTER);
+        buttonPanel.add(bStartGame);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
 
         // Adding Label and Setting the Size of the Frame
-
         frame.setSize(700, 300);
-        
+
         // Making the Frame visible
         frame.setVisible(true);
-        
+
+        // Action listener for Enter key or Button click
+        ActionListener actionListener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        };
+
+        bStartGame.addActionListener(actionListener);
+
         // Using WindowListener for closing the window
         frame.addWindowListener(new WindowAdapter() {
             @Override
@@ -89,6 +103,5 @@ public class TttWindow {
                 System.exit(0);
             }
         });
-        
     }
 }
